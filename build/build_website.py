7fd66a8e6c31583faf0d67a115ff1127ec880578
Sem Essessi <sem.essessi@redmarmosetstudios.com>
2023-08-08 17:12:59 +0100

    fill out images after filtering from generated image data lookup, based on the sources json file

--- build/build_website.py
+++ build/build_website.py
@@ -1,5 +1,47 @@
-# build script for website
-# writes back to the original source html file so that we only regenerate the
-# automatic parts
+# build script for website stuff that isn't the cached images
 
-# does nothing for now.
+import json
+
+from os.path import exists
+
+# start of script
+dataPath = './source/sources.json';
+if exists(dataPath) == False:
+    sys.exit(-1)
+
+dataFile = open(dataPath)
+data = json.load(dataFile)
+dataFile.close()
+
+# build a map from gardiner codes to lists of structured data
+imageDataLookup = {}
+for row in data:
+    # process_row(row, lastSource)
+    if row["glyph"] not in imageDataLookup:
+        imageDataLookup[row["glyph"]] = []
+    imageDataLookup[row["glyph"]].append(row) 
+
+lookupData = "const imageDataLookup = new Map([\n";
+for glyphKey in imageDataLookup:
+    lookupData += '\t["' + glyphKey + '",\n\t\t[\n'
+    rows = imageDataLookup[glyphKey]
+    for row in rows:
+        lookupData += '\t\t\t{\n'
+        lookupData += '\t\t\t\tglyph: "' + glyphKey + '",\n'
+        lookupData += '\t\t\t\tsourceURL: "' + row["original-source-link"] + '",\n'
+        lookupData += '\t\t\t\tlicense: "' + row["license"] + '",\n'
+        lookupData += '\t\t\t\tcredit: "' + row["credit"] + '",\n'
+        lookupData += '\t\t\t\timageURL: "' + row["cached-image"] + '"\n'
+        lookupData += '\t\t\t},\n'
+    # kill last comma
+    lookupData = lookupData[:-2] + '\n'
+    lookupData += '\t\t]\n\t],\n'
+# kill last comma
+lookupData = lookupData[:-2] + '\n'
+lookupData += "]);\n";
+
+# write the file out with the lookup table
+lookupPath = './scripts/imageDataLookup.js'
+lookupFile = open(lookupPath, 'w')
+lookupFile.write(lookupData)
+lookupFile.close()
---
+++ scripts/imageDataLookup.js
@@ -0,0 +1,139 @@
+const imageDataLookup = new Map([
+	["A1",
+		[
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/113750603.png"
+			},
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/809658245.png"
+			},
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/534365659.png"
+			},
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/496299935.png"
+			},
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/688262318.png"
+			},
+			{
+				glyph: "A1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Mastaba_de_Ti_-_jerogl%C3%ADfico_pene.JPG",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Hispalois",
+				imageURL: "./cache/269327850.png"
+			}
+		]
+	],
+	["D4",
+		[
+			{
+				glyph: "D4",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/165227815.png"
+			},
+			{
+				glyph: "D4",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Birth_and_Throne_cartouches_of_pharaoh_Seti_I%2C_from_KV17_at_the_Valley_of_the_Kings%2C_Egypt._Neues_Museum.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Osama Shukir Muhammed Amin FRCP(Glasg)",
+				imageURL: "./cache/442819291.png"
+			}
+		]
+	],
+	["D35",
+		[
+			{
+				glyph: "D35",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Egypte_louvre_144_hieroglyphes.jpg",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Guillaume Blanchard, July 2004, Fujifilm S6900",
+				imageURL: "./cache/450210647.png"
+			}
+		]
+	],
+	["G1",
+		[
+			{
+				glyph: "G1",
+				sourceURL: "https://media.britishmuseum.org/media/Repository/Documents/2014_10/3_21/dcb2aa0f_7594_4b2e_b02e_a3b9015cec32/mid_00228263_001.jpg",
+				license: "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
+				credit: "British Museum",
+				imageURL: "./cache/668283156.png"
+			}
+		]
+	],
+	["G43",
+		[
+			{
+				glyph: "G43",
+				sourceURL: "https://media.britishmuseum.org/media/Repository/Documents/2014_10/3_21/dcb2aa0f_7594_4b2e_b02e_a3b9015cec32/mid_00228263_001.jpg",
+				license: "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
+				credit: "British Museum",
+				imageURL: "./cache/413531474.png"
+			}
+		]
+	],
+	["G54",
+		[
+			{
+				glyph: "G54",
+				sourceURL: "https://media.britishmuseum.org/media/Repository/Documents/2014_10/3_21/dcb2aa0f_7594_4b2e_b02e_a3b9015cec32/mid_00228263_001.jpg",
+				license: "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
+				credit: "British Museum",
+				imageURL: "./cache/281719668.png"
+			}
+		]
+	],
+	["X1",
+		[
+			{
+				glyph: "X1",
+				sourceURL: "https://media.britishmuseum.org/media/Repository/Documents/2014_10/3_21/dcb2aa0f_7594_4b2e_b02e_a3b9015cec32/mid_00228263_001.jpg",
+				license: "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
+				credit: "British Museum",
+				imageURL: "./cache/456632861.png"
+			},
+			{
+				glyph: "X1",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Narmer-Tjet2.JPG",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "NebMaatRa",
+				imageURL: "./cache/828194228.png"
+			}
+		]
+	],
+	["L2",
+		[
+			{
+				glyph: "L2",
+				sourceURL: "https://upload.wikimedia.org/wikipedia/commons/3/3c/AncientEgyptianRelief-BeeHieroglyph-ROM.png",
+				license: "CC Attribution-ShareAlike 3.0 Unported",
+				credit: "Keith Schengili-Roberts",
+				imageURL: "./cache/787098606.png"
+			}
+		]
+	]
+]);
